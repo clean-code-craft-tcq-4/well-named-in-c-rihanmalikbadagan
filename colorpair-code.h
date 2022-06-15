838b3bf48e6aa909de4a90d893a9e03024b986d8
Rihan Malik <rihannb@yahoo.in>
2022-06-15 18:19:43 +0530

    Resolving compiler errors

--- colorpair-code.h
+++ colorpair-code.h
@@ -4,6 +4,19 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
